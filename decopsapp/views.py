3c901c00af0d13b3465c6351f08adb3824f779d3
Arjun R S <arjunrs003@gmail.com>
2025-12-25 12:32:27 +0530

    added glass template in home page under about us section

--- decopsapp/views.py
+++ decopsapp/views.py
@@ -3,10 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
-
-
-def home_page(request):
     return render(request,"index.html")
 
 def about_us(request):
