8d7e3f16f4748e576268009b3bdf29eb9d8a02b2
Arjun R S <arjunrs003@gmail.com>
2026-01-18 20:25:56 +0530

    removed about us

--- decopsapp/urls.py
+++ decopsapp/urls.py
@@ -5,6 +5,5 @@ from decopsapp import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path("about_us", views.about_us, name="about_us"),
     path('contact_Submit', views.contact_submit, name='contact_submit'),
 ] 
